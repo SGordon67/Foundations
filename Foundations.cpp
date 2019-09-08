1647ff9e416b9bc890f0568024becd5257448df6
sgordon130 <sgordon031@gmail.com>
2019-09-08 13:48:20 -0400

    Add project files.

---
+++ Foundations.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <list>
+
+class string {
+};
+class Object {
+public:
+	Object(char x) {
+		this->a = x;
+	}
+	char a;
+};
+class MTString : public string {
+	MTString() {
+
+	}
+};
+class OneString : public string {
+	OneString(Object a, string b) {
+
+	}
+
+};
+
+int main()
+{
+	std::list<Object> myList;
+	Object zero = Object('0');
+	Object one = Object('1');
+	myList.push_back(zero);
+	myList.push_back(one);
+
+	std::cout << "this is the first item\n";
+
+	for (auto v : myList) {
+		std::cout << v.a << "\n";
+	}
+}
