c98937dabcee472173620c248f42b82499e418f2
sgordon130 <sgordon031@gmail.com>
2019-09-15 21:37:20 -0400

    Deleted Extra commented code, Cleaned up print() functions on string classes.

--- Foundations.cpp
+++ Foundations.cpp
@@ -46,6 +46,7 @@ public:
 	virtual bool isEmpty() { return true; }
 	virtual Char fChar() { return (Char('n')); }
 	virtual String* next() { return (&String()); }
+	virtual void print() {};
 };
 
 class epsilon : public String {
@@ -54,6 +55,7 @@ public:
 	bool isEmpty() { return true; }
 	Char fChar() { throw new _exception; }
 	String* next() { throw new _exception; }
+	void print() { std::cout << 'E'; }
 };
 
 class OneString : public String{
@@ -76,38 +78,10 @@ public:
 
 	void print() {
 		std::string a;
-
-		while ( this->isEmpty() == false ) {
-			//std::cout << "im here";
-			a.push_back(this->c.c);
-			a.push_back(this->next()->fChar().c);
-			break;
-		}
-		std::cout << a;
-		//std::cout << this->c.c;
+		std::cout << this->c.c;
+		this->s->print();
 	}
 };
-
-
-/*
-class string {
-};
-class OneString {
-public:
-	OneString() {}
-	OneString(Object x, OneString* y) {
-		this->a = x;
-		this->b = y;
-	}
-	Object a;
-	OneString* b;
-};
-class MTString : public OneString {
-public:
-	MTString() {}
-};
-*/
-
 
 int main()
 {
@@ -116,37 +90,7 @@ int main()
 	Char test3 = Char('s');
 
 	epsilon e = epsilon();
-	OneString testString = OneString(test, &OneString(test2,  &e));
+	OneString testString = OneString(test, &OneString(test2,  &OneString(test3, &OneString(test, &e))));
 
 	testString.print();
-
-	/*
-	MTString mt = MTString();	// Empty String
-
-	Object sigma = Object("sigma");
-	Object zero = Object("0");	// characters
-	Object one = Object("1");
-
-	Alphabet.push_back(zero);
-	Alphabet.push_back(one);
-
-	std::cout << "The Alphabet: \n"; // Outputting the alphabet
-	for (auto v : Alphabet) {
-		std::cout << v.o << "\n";
-	}
-
-	// iterate through the alphabet to grab characters 
-	std::list<Object>::iterator it = Alphabet.begin();
-	Object temp = *it;
-	std::cout << "first element: " << temp.o << "\n";
-	// grab a specific element of the list
-	std::list<Object>::iterator it1 = std::next(Alphabet.begin(), 1);
-	temp = *it1;
-	std::cout << "Second element: " << temp.o << "\n";
-
-	// outputting a specific string 
-	OneString OIO = OneString(zero, new OneString(one, new OneString(zero, new MTString())));
-	std::cout << "OIO string: \n";
-	std::cout << OIO.a.o << "\n" <<  OIO.b->a.o << "\n" << OIO.b->b->a.o << "\n";
-	*/
 }
